0a862af2cc7b86ae6ee1cec085c2e3100adc17d3
zsoltibv <zsoltibv@gmail.com>
2023-05-26 02:00:05 +0300

    Solved Task

---
+++ Minesweeper/Models/GridElement.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Minesweeper.Models
+{
+    public class GridElement : BaseNotification
+    {
+        public int Row { get; set; }
+        public int Col { get; set; }
+
+        private Visibility _isVisible;
+        public Visibility IsVisible
+        {
+            get { return _isVisible; }
+            set
+            {
+                _isVisible = value;
+                NotifyPropertyChanged(nameof(IsVisible));
+            }
+        }
+
+        private bool _isBomb;
+        public bool IsBomb
+        {
+            get { return _isBomb; }
+            set
+            {
+                _isBomb = value;
+                NotifyPropertyChanged(nameof(IsBomb));
+            }
+        }
+
+        private int _nrOfBombs;
+        public int NrOfBombs
+        {
+            get { return _nrOfBombs; }
+            set
+            {
+                _nrOfBombs = value;
+                NotifyPropertyChanged(nameof(NrOfBombs));
+            }
+        }
+
+        private string _content;
+        public string Content
+        {
+            get { return _content; }
+            set
+            {
+                _content = value;
+                NotifyPropertyChanged(nameof(Content));
+            }
+        }
+
+        private string _color;
+        public string Color
+        {
+            get { return _color; }
+            set
+            {
+                _color = value;
+                NotifyPropertyChanged(nameof(Color));
+            }
+        }
+
+        public GridElement(int row, int col)
+        {
+            Row = row;
+            Col = col;
+            IsVisible = Visibility.Collapsed;
+            IsBomb = false;
+            NrOfBombs = 0;
+            Content = string.Empty;
+            Color = "gray";
+        }
+    }
+}
---
+++ Minesweeper/ViewModels/BoardViewModel.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Xml;
+using System.Xml.Linq;
+using Minesweeper.Models;
+
+namespace Minesweeper
+{
+    public class BoardViewModel : BaseNotification
+    {
+        private readonly int numRows = 9;
+        private readonly int numCols = 9;
+        private readonly int nrOfBombs = 10;
+
+        int[] rowOffsets = { -1, -1, -1, 0, 0, 1, 1, 1 };
+        int[] colOffsets = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+        private ObservableCollection<GridElement> _grid;
+        public ObservableCollection<GridElement> Grid
+        {
+            get { return _grid; }
+            set
+            {
+                _grid = value;
+                NotifyPropertyChanged(nameof(Grid));
+            }
+        }
+
+        public BoardViewModel()
+        {
+            GenerateGrid();
+            GenerateBombs();
+            GenerateHelperNumbers();
+        }
+
+        private void GenerateGrid()
+        {
+            Grid = new ObservableCollection<GridElement>();
+            for (int row = 0; row < numRows; row++)
+            {
+                for (int col = 0; col < numCols; col++)
+                {
+                    var button = new GridElement(row, col);
+                    Grid.Add(button);
+                }
+            }
+        }
+
+        private void GenerateBombs()
+        {
+            int count = 0;
+            var random = new Random();
+
+            while (count < nrOfBombs)
+            {
+                int randomPos = random.Next(0, numRows * numCols);
+
+                if (!Grid[randomPos].IsBomb)
+                {
+                    Grid[randomPos].IsBomb = true;
+                    Grid[randomPos].Content = "Bomb";
+                    count++;
+                }
+            }
+        }
+
+        private void GenerateHelperNumbers()
+        {
+            for (int i = 0; i < numRows * numCols; i++)
+            {
+                GridElement element = Grid[i];
+
+                for (int j = 0; j < 8; j++)
+                {
+                    int neighborRow = element.Row + rowOffsets[j];
+                    int neighborCol = element.Col + colOffsets[j];
+
+                    if (neighborRow < 0 || neighborRow >= numRows || neighborCol < 0 || neighborCol >= numCols)
+                    {
+                        continue;
+                    }
+
+                    int neighborIndex = neighborRow * numCols + neighborCol;
+                    GridElement neighbor = Grid[neighborIndex];
+
+                    if (element.IsBomb == true && neighbor.IsBomb == false)
+                    {
+                        neighbor.NrOfBombs++;
+                        neighbor.Content = neighbor.NrOfBombs.ToString();
+                    }
+                }
+            }
+        }
+
+        public void ShowAllBombs()
+        {
+            for (int i = 0; i < numRows * numCols; i++)
+            {
+                if (Grid[i].IsBomb == true)
+                {
+                    Grid[i].IsVisible = Visibility.Visible;
+                    Grid[i].Color = "white";
+                }
+            }
+        }
+
+        public void MakeVisible(object param)
+        {
+            if (param is GridElement element)
+            {
+                if (element.Color != "white")
+                {
+                    element.IsVisible = Visibility.Visible;
+                    element.Color = "white";
+
+                    if (element.IsBomb == true)
+                    {
+                        ShowAllBombs();
+                        MessageBox.Show("You lost!");
+                    }
+                    else if (element.Content == "")
+                    {
+                        MakeVisibleAround(element.Row, element.Col);
+                    }
+                }
+            }
+            if (CheckWin())
+            {
+                MessageBox.Show("You won!");
+                return;
+            }
+        }
+
+        public void MakeVisibleAround(int startRow, int startCol)
+        {
+            for (int row = startRow - 1; row <= startRow + 1; row++)
+            {
+                for (int col = startCol - 1; col <= startCol + 1; col++)
+                {
+                    if (row >= 0 && row < numRows && col >= 0 && col < numCols)
+                    {
+                        MakeVisible(Grid[row * numCols + col]);
+                    }
+                }
+            }
+        }
+
+        public bool CheckWin()
+        {
+            for (int i = 0; i < numRows * numCols; i++)
+            {
+                GridElement element = Grid[i];
+                if (!element.IsBomb && element.IsVisible != Visibility.Visible)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public ICommand MakeVisibleCommand => new RelayCommand<object>(MakeVisible);
+    }
+}
